95095d0629d4c4c73b58a1bc63208c644d5e4a48
gasan <abduragimov.ga@gmail.com>
2022-09-27 10:20:14 +0500

    15.4 Кластеры. Ресурсы под управлением облачных провайдеров

--- 15.4/README.md
+++ 15.4/README.md
@@ -1,10 +1,19 @@
-# 15.3 Безопасность в облачных провайдерах
+# 15.4 Кластеры. Ресурсы под управлением облачных провайдеров
 
-## Encrypted Object Storage<br>
-   [Main](https://github.com/Gasan66/devops-netology/blob/main/15.3/main.tf)
-
-## HTTPS Site <br>
+## MySQL cluster<br>
    <kbd> 
-      <img src="https://github.com/Gasan66/devops-netology/blob/main/15.3/https.png" alt="https"
-      title="https"/> 
+      <img src="https://github.com/Gasan66/devops-netology/blob/main/15.4/mysql/cluster.png" alt="cluster"
+      title="cluster"/> 
+   </kbd>
+
+## DB <br>
+   <kbd> 
+      <img src="https://github.com/Gasan66/devops-netology/blob/main/15.4/mysql/database.png" alt="database"
+      title="database"/> 
+   </kbd>
+
+## K8S <br>
+   <kbd> 
+     <img src="https://github.com/Gasan66/devops-netology/blob/main/15.4/k8s/k8s.png" alt="database"
+     title="database"/> 
    </kbd>
--- 15.4/k8s/main.tf
+++ 15.4/k8s/main.tf
@@ -12,7 +12,7 @@ provider "yandex" {
   token     = var.yandex_cloud_token
   cloud_id  = "b1g32us03hkhaselg0p1"
   folder_id = "b1gef0er4511elohdc2q"
-  zone      = "ru-central1-a"
+  zone      = "ru-central1-b"
 }
 
 resource "yandex_vpc_network" "net-netology" {
@@ -25,68 +25,150 @@ resource "yandex_vpc_subnet" "public" {
   network_id     = yandex_vpc_network.net-netology.id
 }
 
-resource "yandex_vpc_subnet" "private" {
+resource "yandex_vpc_subnet" "private1" {
   v4_cidr_blocks = ["192.168.20.0/24"]
   zone           = "ru-central1-b"
   network_id     = "${yandex_vpc_network.net-netology.id}"
 }
 
-resource "yandex_mdb_mysql_cluster" "db_cluster" {
-  name        = "my_first_cluster"
-  environment = "PRESTABLE"
-  network_id  = yandex_vpc_network.net-netology.id
-  version     = "8.0"
-  deletion_protection = true
-
-  resources {
-    resource_preset_id = "b1.medium"
-    disk_type_id       = "network-ssd"
-    disk_size          = 20
-  }
-
-  maintenance_window {
-    type = "ANYTIME"
-  }
-
-  backup_window_start {
-    hours = 23
-    minutes = 59
-  }
-
-  host {
-    zone      = "ru-central1-a"
-    subnet_id = yandex_vpc_subnet.public.id
-  }
-
-  host {
-    zone      = "ru-central1-b"
-    subnet_id = yandex_vpc_subnet.private.id
-  }
+resource "yandex_vpc_subnet" "private2" {
+  v4_cidr_blocks = ["192.168.30.0/24"]
+  zone           = "ru-central1-c"
+  network_id     = "${yandex_vpc_network.net-netology.id}"
 }
 
-resource "yandex_mdb_mysql_database" "my_database" {
-  cluster_id = yandex_mdb_mysql_cluster.db_cluster.id
-  name       = "netology_db"
+resource "yandex_kms_symmetric_key" "key-k8s" {
+  name              = "netology-symetric-key-k8s"
+  description       = "k8s key"
+  default_algorithm = "AES_128"
+  rotation_period   = "8760h" // equal to 1 year
 }
 
-resource "yandex_mdb_mysql_user" "my_user" {
-    cluster_id = yandex_mdb_mysql_cluster.db_cluster.id
-    name       = "my_first_user"
-    password   = "password"
+resource "yandex_kubernetes_cluster" "regional_cluster_resource_name" {
+  name        = "my-k8s-cluster"
+  description = "description"
 
-    permission {
-      database_name = yandex_mdb_mysql_database.my_database.name
-      roles         = ["ALL"]
+  network_id = "${yandex_vpc_network.net-netology.id}"
+
+  master {
+    regional {
+      region = "ru-central1"
+
+      location {
+        zone      = "${yandex_vpc_subnet.public.zone}"
+        subnet_id = "${yandex_vpc_subnet.public.id}"
+      }
+
+      location {
+        zone      = "${yandex_vpc_subnet.private1.zone}"
+        subnet_id = "${yandex_vpc_subnet.private1.id}"
+      }
+
+      location {
+        zone      = "${yandex_vpc_subnet.private2.zone}"
+        subnet_id = "${yandex_vpc_subnet.private2.id}"
+      }
     }
 
-    connection_limits {
-      max_questions_per_hour   = 10
-      max_updates_per_hour     = 20
-      max_connections_per_hour = 30
-      max_user_connections     = 40
+    public_ip = true
+
+    maintenance_policy {
+      auto_upgrade = true
+
+      maintenance_window {
+        day        = "monday"
+        start_time = "15:00"
+        duration   = "3h"
+      }
+
+      maintenance_window {
+        day        = "friday"
+        start_time = "10:00"
+        duration   = "4h30m"
+      }
+    }
+  }
+
+  service_account_id      = var.service_account_id
+  node_service_account_id = var.service_account_id
+
+  labels = {
+    my_key       = "my_value"
+    my_other_key = "my_other_value"
+  }
+
+  kms_provider {
+    key_id = "${yandex_kms_symmetric_key.key-k8s.id}"
+  }
+
+  release_channel = "STABLE"
+}
+
+resource "yandex_kubernetes_node_group" "my_node_group" {
+  cluster_id  = "${yandex_kubernetes_cluster.regional_cluster_resource_name.id}"
+  name        = "my-first-node-group"
+  description = "description"
+
+  labels = {
+    "key" = "value"
+  }
+
+  instance_template {
+    platform_id = "standard-v2"
+
+    network_interface {
+      nat                = true
+      subnet_ids         = ["${yandex_vpc_subnet.public.id}"]
     }
 
-    global_permissions = ["PROCESS"]
+    resources {
+      memory = 2
+      cores  = 2
+    }
 
-    authentication_plugin = "SHA256_PASSWORD"
+    boot_disk {
+      type = "network-hdd"
+      size = 64
+    }
+
+    scheduling_policy {
+      preemptible = false
+    }
+
+    container_runtime {
+      type = "containerd"
+    }
+  }
+
+  scale_policy {
+    auto_scale {
+      min = 3
+      max = 6
+      initial = 3
+    }
+
+  }
+
+  allocation_policy {
+    location {
+      zone = "ru-central1-a"
+    }
+  }
+
+  maintenance_policy {
+    auto_upgrade = true
+    auto_repair  = true
+
+    maintenance_window {
+      day        = "monday"
+      start_time = "15:00"
+      duration   = "3h"
+    }
+
+    maintenance_window {
+      day        = "friday"
+      start_time = "10:00"
+      duration   = "4h30m"
+    }
+  }
 }
